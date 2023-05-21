03e9ddf6c9d1e18ddf0d478e64a3871f678404ef
Doug Emery <emeryr@upenn.edu>
2023-05-21 09:26:59 -0400

    Script to run initial homebrew setup

---
+++ setup_homebrew.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+shopt -s expand_aliases
+source $(dirname $0)/functions.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+HOMEBREW_PACKAGES="rbenv ruby-build coreutils tree vim git bash bash-completion pyenv npm dos2unix"
+
+if which -s brew
+then
+  msg "Homebrew already installed"
+else
+  msg "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+packages_to_install=
+installed=$(brew list)
+for pkg in $HOMEBREW_PACKAGES
+do
+  echo "$installed" | egrep -q "\b$pkg\b" || packages_to_install="${packages_to_install} $pkg"
+done
+
+if [[ -n "${packages_to_install}" ]]
+then
+  msg "Installing ${packages_to_install}"
+  brew install ${packages_to_install}
+else
+  msg "No packages to install"
+fi
+
+outdated=$(brew outdated)
+if [[ -n "${outdated}" ]]
+then
+  msg "Upgrading outdated packages: $(echo ${outdated})"
+  brew upgrade ${outdated}
+else
+  msg "No packages are outdated"
+fi
+
+# install the latest ruby
+ruby_version=$(rbenv install --list | egrep "^\d" | tail -1)
+
+if rbenv versions | grep -q ${ruby_version}
+then
+  msg "Ruby version ${ruby_version} already installed"
+else
+  msg "Installing Ruby version ${ruby_version}"
+  rbenv install ${ruby_version}
+fi
+
+# set up python
+
+# install the latest python
+python_version=$(pyenv install --list | egrep "^\s+\d\.\d+\.\d+$" | tail -1)
+
+if pyenv versions | grep -q ${python_version}
+then
+  msg "Python version ${python_version} already installed"
+else
+  msg "Installing Python version ${python_version}"
+  pyenv install ${python_version}
+fi
+
